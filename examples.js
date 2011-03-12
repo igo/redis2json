2ee169840dc883323b8b32979f3bacf402d42f68
Igor Urminček <igo@inspired.sk>
2011-03-12 18:27:34 +0000

    example fixes

--- examples.js
+++ examples.js
@@ -11,42 +11,38 @@ var map = {
 	postId: ":{postId}",
 
 	text: "post:{postId}:text",
-		created: "post:{postId}:created",
-		$authorId: "post:{postId}:author",
-		author: {
-			authorId: ":{authorId}",
-			username: "user:{authorId}:username",
-			password: "user:{authorId}:password"
-		},
-			$$comments: ["commentId", "lrange", "post:{postId}:comments", 1, 20],
-
+    created: "post:{postId}:created",
+    $authorId: "post:{postId}:author",
+    author: {
+        authorId: ":{authorId}",
+        username: "user:{authorId}:username",
+        password: "user:{authorId}:password"
+    },
 	$$comments: { variable: "commentId", cmd: "lrange", key: "post:{postId}:comments", args: [0, 20] },
-		comments: [
-							{
-								commentId: ":{commentId}",
-								text: "comment:{commentId}:text",
-			
+	comments: [
+		{
+			commentId: ":{commentId}",
+			text: "comment:{commentId}:text",
 			created: "comment:{commentId}:create",
-						$commentAuthorId: "comment:{commentId}:author",
-						author: {
-							authorId: ":{commentAuthorId}",
-							username: "user:{commentAuthorId}:username",
-							password: "user:{commentAuthorId}:password"
-						}
-
+			$commentAuthorId: "comment:{commentId}:author",
+			author: {
+				authorId: ":{commentAuthorId}",
+				username: "user:{commentAuthorId}:username",
+				password: "user:{commentAuthorId}:password"
+			}
 		}
 	]
-}
+};
 
 map = {
 	$$posts: { variable: "postId", cmd: "lrange", key: "posts", args: [0, 30] },
 	posts: [ map ]
-}
+};
 
 
 var variables = {
 	postId: 1
-}
+};
 
 
 redis2json.load(map, variables, function (error, object) {
